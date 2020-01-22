11a568ba463d0ab292c802407df48ffa80b0913a
ahmed emad <ahmed.3m4d@gmail.com>
2020-01-22 17:25:31 +0200

    .

--- js/babel.jsx
+++ js/babel.jsx
@@ -2,24 +2,19 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      link: '',
-      status: 0
+      link: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
   handleChange(event){
     this.setState({
-      status: this.state.status += 1,
       link: event.target.value
     });
-    if(this.state.status >= 1){
       $("#box").append("<button id='test'>new link</button><br>").on({
         'click': this.handleClick
       });
-      document.getElementById("test").value = event.target.value;
-    }
-    
+      document.getElementById("test").value = event.target.value;    
   };
   handleClick(event){
     this.setState({
@@ -29,16 +24,19 @@ class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <div className='col-lg-9 col-md-9'>
+        <row>
+          <h3 className='text-center'>write your link in embed</h3>
+        </row>
+        <div className='col-lg-9 col-md-7 col-sm-12 col-xs-12'>
             <div className='row'>
-              <input id='textArea' value={this.state.input} onChange={this.handleChange} />
+              <input id='textArea' onChange={this.handleChange} />
             </div>
             <div className='row'>
-              <iframe width="820" height="345" type="text/html" src={this.state.link} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture">
+              <iframe width='100%' height='350' type="text/html" src={this.state.link} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture">
               </iframe>
             </div>
           </div>
-          <div className='col-lg-3 col-md-3' id='box'>
+          <div className='col-lg-3 col-md-5 col-sm-12 col-xs-12' id='box'>
             <div><button value='https://www.youtube.com/embed/CtBS35jx1Ls' onClick={this.handleClick}>link 1</button></div>
             <div><button value='https://www.youtube.com/embed/R8GCc8OhTz8' onClick={this.handleClick}>link 2</button></div>
             <div><button value='https://www.youtube.com/embed/Sv6dMFF_yts' onClick={this.handleClick}>link 3</button></div>
